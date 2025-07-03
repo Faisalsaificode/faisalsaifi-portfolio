46c28c8c5a0d85df8ef53172d220b9876ca5564c
faisalsaificode <faisalsaifi.code@gmail.com>
2025-07-04 00:46:13 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // ✅ Important for custom domain
+  base: "./", // ✅ Important for custom domain
   server: {
     host: "::",
     port: 8080,
